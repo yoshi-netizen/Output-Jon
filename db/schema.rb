2c9efe63c9c1f4a17afe206853db767868d09608
yoshi-netizen <yoshinori.nakagawa5@gmail.com>
2026-04-22 01:45:47 +0900

    Postモデル作成、Userモデルと関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_13_061451) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_21_152709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "thinking_topic", null: false
+    t.text "thinking_diffusion"
+    t.string "thinking_core"
+    t.text "thinking_output"
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +37,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_13_061451) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
