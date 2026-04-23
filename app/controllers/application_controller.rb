d7323f5a8f4846c5066611a6d1c07bf528b6c4e2
yoshi-netizen <yoshinori.nakagawa5@gmail.com>
2026-04-24 07:11:02 +0900

    ログイン後の遷移先を投稿作成ページに設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  def after_sign_in_path_for(resource)
+  # ログイン後の遷移先を投稿作成ページに設定
+  # deviseのデフォルトではルートページに設定されているため
+  def after_sign_in_path_for(resource) 
     new_post_path
   end
 end
