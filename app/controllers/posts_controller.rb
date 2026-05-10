5b6025fdb2d06c08c28a9b7d02ddf20063d650b4
yoshi-netizen <yoshinori.nakagawa5@gmail.com>
2026-05-10 23:28:44 +0900

    コントローラーにeditアクションとupdateアクションを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,20 @@ class PostsController < ApplicationController
     @post = current_user.posts.find(params[:id])
   end
 
+  def edit 
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), success: '投稿を更新しました'
+    else
+      flash.now[:danger] = '更新に失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def post_params  # ストロングパラメータ
