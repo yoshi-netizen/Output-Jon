27d734544a9e496bf7e8d42c1b1b61bf331e0f16
yoshi-netizen <yoshinori.nakagawa5@gmail.com>
2026-05-23 01:04:35 +0900

    rapecエラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:passwords, :registrations]
+  devise_for :users, skip: [ :passwords, :registrations ]
   devise_scope :user do
     get  "/users/sign_up", to: "devise/registrations#new",    as: :new_user_registration
     post "/users",         to: "devise/registrations#create",  as: :user_registration
