70078b22481f8a6169b7861724575e730203ed2f
yoshi-netizen <ossanta64@gmail.com>
2026-06-20 00:36:33 +0900

    Revert "READMEの微修正"

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,5 @@
 
 import { application } from "./application"
 
-import ClipboardController from "./clipboard_controller"
-application.register("clipboard", ClipboardController)
-
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
