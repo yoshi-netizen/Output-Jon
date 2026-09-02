5a4452371865cf2c0edce021c7eacefdc4764e74
yoshi-netizen <yoshinori.nakagawa5@gmail.com>
2026-09-02 14:34:14 +0900

    DBにterms_accepted_atカラム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
+  validates_acceptance_of :agreement, allow_nil: false, on: :create
 end
