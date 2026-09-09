358112728874e0e8dddd36a29ab874d0b385ce68
yoshi-netizen <yoshinori.nakagawa5@gmail.com>
2026-09-10 01:34:23 +0900

    新規登録画面の規約リンク確認テスト作成

--- spec/system/users/registrations_spec.rb
+++ spec/system/users/registrations_spec.rb
@@ -1,22 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "規約同意画面", type: :system do
-  include LoginMacros
+RSpec.describe "新規登録画面", type: :system do
   include NewTabMacros
-  let(:user) { FactoryBot.create(:user) }
 
-  before do
-    user.update_column(:terms_accepted_at, nil)
-    login(user)
-  end
+  before { visit new_user_registration_path }
 
   it "利用規約リンクをクリックすると別タブで利用規約が表示されること" do
-    click_link "利用規約を確認する"
+    within("form") { click_link "利用規約" }
     expect_new_tab_content("利用規約")
   end
 
   it "プライバシーポリシーリンクをクリックすると別タブでプライバシーポリシーが表示されること" do
-    click_link "プライバシーポリシーを確認する"
+    within("form") { click_link "プライバシーポリシー" }
     expect_new_tab_content("プライバシーポリシー")
   end
 end
