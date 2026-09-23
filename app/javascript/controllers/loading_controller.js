2439102fee8b4dff7bd05ac78dae30c361ff71f1
yoshi-netizen <yoshinori.nakagawa5@gmail.com>
2026-09-24 03:18:33 +0900

    微修正

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -4,7 +4,7 @@ export default class extends Controller {
   static targets = ["button", "label", "spinner"]
 
   start(event) {
-    if (event.detail.formSubmission.submitter !== this.buttonTarget) return    // 押されたボタンが「AIで整理する」か確認
+    if (event.detail.formSubmission.submitter !== this.buttonTarget) return    // 押されたボタンが「AIで整理する」か確認。保存ボタンでは実行されない。
     this.labelTarget.classList.add("hidden")
     this.spinnerTarget.classList.remove("hidden")
   }
